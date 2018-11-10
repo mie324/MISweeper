92beb9fe45eccce621d467f25fb8434fc2a79ae1
daniil <daniil.orekhov15@gmail.com>
2018-11-10 17:23:22 -0500

    imlemented parsing loss and optimizer for config sector

--- Config/config_parser.py
+++ Config/config_parser.py
@@ -1,12 +1,15 @@
-
 import json
 
+import torch.optim as optim
+import torch.nn as nn
 
 # TODO implement parsing config for data preparation
 
+
 def load_config():
 
-    config = json.load("config.json")
+    with open("config.json", "r") as fh:
+        config = json.load(fh)
 
     batch_size = config["batch_size"]
     learning_rate = config["learning_rate"]
@@ -14,18 +17,22 @@ def load_config():
     seed = config["seed"]
 
     loss = parse_loss(config["loss"])
-    optimizer = parse_optimizer(config["optimizer"])
+    optimizer = parse_optimizer(config["optimizer"], learning_rate)
 
     return config, learning_rate, batch_size, num_epochs, loss, optimizer, seed
 
 
-def parse_optimizer(json):
-    # TODO implement parse_optimizer() to return optimizer ready for training
+def parse_optimizer(optimizer_config, learning_rate):
+    optimizer = None
+    if optimizer_config["name"] == "adam":
+        optimizer = optim.Adam
 
-    return None
+    return optimizer(lr=learning_rate, **optimizer_config["kwargs"])
 
 
-def parse_loss(json):
-    # TODO implement parse_loss() to return loss function ready for training
+def parse_loss(loss_config):
+    loss = None
+    if loss_config["name"] == "mse":
+        loss = nn.MSELoss
 
-    return None
+    return loss(**loss_config["kwargs"])
