27370a2c10c2f8eb3df54754945cd04ec6c34d28
daniil <daniil.orekhov15@gmail.com>
2018-11-10 14:43:27 -0500

    initial config sector setup

--- Config/config_parser.py
+++ Config/config_parser.py
@@ -1,3 +1,32 @@
 
-def load_config():
-    pass
+import json
+
+
+# TODO implement parsing config for data preparation
+
+def load_config(path):
+
+    with open(path) as file:
+        config = json.load(file)
+
+    batch_size = config["batch_size"]
+    learning_rate = config["learning_rate"]
+    num_epochs = config["num_epochs"]
+    seed = config["seed"]
+
+    loss = parse_loss(config["loss"])
+    optimizer = parse_optimizer(config["optimizer"])
+
+    return config, learning_rate, batch_size, num_epochs, loss, optimizer, seed
+
+
+def parse_optimizer(json):
+    # TODO implement parse_optimizer() to return optimizer ready for training
+
+    return None
+
+
+def parse_loss(json):
+    # TODO implement parse_loss() to return loss function ready for training
+
+    return None
