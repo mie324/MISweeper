d55d46185a5469da944f16ddba88604acfc7b4ab
daniil <daniil.orekhov15@gmail.com>
2018-11-26 14:46:27 -0500

    moved confusion matrix plotting to evaluation handler

--- evaluation_handler.py
+++ evaluation_handler.py
@@ -3,7 +3,8 @@ from Results.results_handler import ResultsHandler
 import numpy as np
 import torch
 from sklearn.metrics import confusion_matrix
-from Results.results_parser import plot_confusion_matrix
+import matplotlib.pyplot as plt
+import itertools
 import os
 
 
@@ -93,6 +94,45 @@ class EvaluationHandler:
             true_labels = np.append(labels, true_labels)
 
         cm = confusion_matrix(true_labels, predictions)
-        plot_confusion_matrix(os.path.join(self.results_handler.dst_path, 'confusion_matrix.png'), cm, normalize=True)
+        self.plot_confusion_matrix(os.path.join(self.results_handler.dst_path, 'cm.png'), cm, normalize=True)
 
+    def plot_confusion_matrix(self, path, cm,
+                              normalize=False,
+                              title='Confusion matrix',
+                              cmap=plt.cm.Blues):
+        """
+        This function prints and plots the confusion matrix.
+        Normalization can be applied by setting `normalize=True`.
 
+        Args:
+            path: String to the output folderin which to save the cm image
+            cm: Numpy array of confusion matrix
+            classes: List of strings of names of the classes
+            normalize: Whether or not to normalize the cm
+            title: Does what you think
+            cmap: color map to use
+        """
+
+        if normalize:
+            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+        fig = plt.figure()
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        classes = [str(i) for i in [6, 15, 16, 42, 52, 53, 62, 64, 65, 67, 88, 90, 92, 95]]
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes)
+        plt.yticks(tick_marks, classes)
+
+        fmt = '.1f' if normalize else 'd'
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, format(cm[i, j], fmt),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.tight_layout()
+        plt.savefig(path)
