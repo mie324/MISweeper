4124a6fe601f4888fadbb5f279a44bab51917f93
Graham Hoyes <accounts@grahamhoyes.com>
2018-11-27 20:30:59 -0500

    Useful messages and load object ids

--- prediction_generator.py
+++ prediction_generator.py
@@ -5,6 +5,9 @@ from Data.dataset import LSSTDataset
 from torch.utils.data import DataLoader
 from model import Net
 from Config.config_parser import *
+import time
+
+start_time = time.time()
 
 device = get_device()
 
@@ -13,21 +16,32 @@ class_names = list(sample_sub.columns[1:-1])
 submission_columns = list(sample_sub.columns)
 del sample_sub
 
+print('Loading the dataset...')
 dataset = np.load('Data/RawData/test_set.npz')
 data = dataset['data']
 lengths = dataset['lengths']
 full_test_ss = dataset['stats']
+print('Done.')
 
 # Create the data loader
+print('Creating data loader...')
 test_dataset = LSSTDataset(full_test_ss, data, None, lengths)
 test_loader = DataLoader(test_dataset, batch_size=get_batch_size())
+print('Done.')
 
 net = Net().to(device)
 net.load_state_dict(torch.load('Results/Graham-PC-Ubuntu/combined_balanced_1/model.pt'))
 
+print('Generating predictions...')
 all_preds = None  # Store the predictions
 
-for pred_data in test_loader:
+test_loader_len = len(test_loader)
+
+object_ids = np.load('Data/RawData/object_ids.npy')
+
+for idx, pred_data in enumerate(test_loader):
+    if idx % 10 == 0:
+        print('Batch %d/%d' % (idx, test_loader_len))
     stats, time_series, lengths = pred_data
 
     time_series = time_series.float().to(device) if type(time_series) != list else [inp.float().to(device) for inp
@@ -48,13 +62,17 @@ for pred_data in test_loader:
     else:
         all_preds = np.vstack((all_preds, predictions))
 
+print('Saving predictions')
 # Save the predictions
 final_predictions = pd.DataFrame(all_preds)
-final_predictions.insert(loc=0, column='object_id', value=test_stats.index.values)
+final_predictions.insert(loc=0, column='object_id', value=object_ids)
 final_predictions.insert(loc=len(final_predictions.columns), column='class_99', value=0)
 final_predictions.columns = submission_columns
 
 final_predictions.to_csv('Results/predictions.csv', header=True, mode='w', index=False)
+print('Done.')
+end_time = time.time()
+print('Time elapsed: %d' % int(end_time - start_time))
 
 
 
